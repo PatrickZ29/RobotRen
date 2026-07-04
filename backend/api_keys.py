330ea0edbdcceb4aa1708dcf189f99a59ecce4e7
PatrickZ29 <148374193+PatrickZ29@users.noreply.github.com>
2026-07-03 22:56:35 -0500

    Update api_keys.py

--- backend/api_keys.py
+++ backend/api_keys.py
@@ -1,6 +1,11 @@
-API_KEY = "AIzaSyABbri8O_8IDENni5qHZxImoC_CFCHsIoY"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 def get_api_key():
-    if not API_KEY:
-        raise Exception("API KEY no configurada")
-    return API_KEY
+    api_key = os.getenv("GEMINI_API_KEY")
+    if not api_key:
+        raise Exception("GEMINI_API_KEY no configurada en variables de entorno")
+    return api_key
